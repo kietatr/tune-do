be1e27bc1af058498c0db75f9d1f17bf833ad922
Kiet Tran <kietanhtran38@gmail.com>
2019-03-28 02:11:34 -0500

    hover to play audio: in progress

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import './shared/SharedStyles.css';
 import './Todo.css';
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaVolume } from "react-icons/fa";
+
+import Sound from 'react-sound';
+
+import soundfile from "./assets/sounds/Xylophone.hardrubber.ff.A4.stereo.mp3";
 
 export default class Todo extends Component {
+  state = {
+    playSound: true
+  }
+
+  playSound = () => {
+    this.setState({ playSound: true })
+  }
+
   render() {
     return (
-      <li className="Todo box center">
+      <li className="Todo box center" onMouseEnter={this.playSound}>
         <button className="icon center">
           <FaCheck size={20} />
         </button>
-        <span className="music">{this.props.value}</span>
+        <span>
+          {this.props.value}
+        </span>
+        <FaVolume className="volume-icon" size={20} />
+        {this.state.playSound &&
+          <Sound
+            url={soundfile}
+            playStatus={Sound.status.PLAYING}
+          />
+        }
       </li>
     );
   }
